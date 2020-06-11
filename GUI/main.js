0644617d211abcd0c65aeff2edc59eea61dc0848
mukul <mukul47@gitlab.com>
2020-06-11 22:03:21 +0530

    screen size issue fixed

--- GUI/main.js
+++ GUI/main.js
@@ -1,23 +1,16 @@
 const {app, BrowserWindow, webFrame, screen, ipcMain} = require('electron')
-function createWindow () {
-  
-  // loadURL(require('url').format({
-  //   pathname: __dirname+'/a.html',
-  //   protocol: 'file:',
-  //   slashes: true
-  // }))//'../index.html')//loadFile('a.html')
-}
+
 app.whenReady().then(() => createWindow())
+
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
 
 app.on('ready', function() {
   const mainWindow = new BrowserWindow({show : false, webPreferences: { nodeIntegration: true }})
   mainWindow.maximize();
   mainWindow.show();
-  // mainWindow.setMenu(null)
+  mainWindow.setMenu(null)
   let dirname = __dirname.split('/')
   dirname.pop()
   dirname = 'file://'+ dirname.join('/') + '/index.html'
